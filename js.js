77d5d0c531663b3dcf9e9df20dba31bf3f5a14e4
malackapite <veress.marcell@protonmail.com>
2023-02-22 01:37:58 +0100

    book :cat2::dash:

--- js.js
+++ js.js
@@ -1,20 +1,25 @@
 hol = 0;
+
+for(var ix=1; ix < document.getElementsByClassName("oldal").length-1; ix++){
+  document.getElementsByClassName("jobb")[document.getElementsByClassName("oldal").length-1-ix].style.width=400-2*ix+"px";
+  document.getElementsByClassName("bal")[ix].style.width=400-2*ix+"px";
+}
+
 konyv=document.getElementsByClassName("oldal")
 function lapoz(nth) {
   if (nth == hol) {
-    konyv[hol].style.transform = "rotateY(180deg) rotateX(10deg)";
-    konyv[hol].getElementsByTagName("div")[0].style.transform = "rotateY(180deg) rotateX(10deg)";
-    konyv[hol].getElementsByTagName("div")[1].style.transform = "rotateY(0deg) rotateX(10deg)";
+    konyv[hol].style.transform = "rotateY(180deg)";
+    konyv[hol].getElementsByTagName("div")[0].style.transform = "rotateY(-180deg)";
+    konyv[hol].getElementsByTagName("div")[1].style.transform = "rotateY(0deg)";
     konyv[hol].style.zIndex = hol;
     hol++;
   } else {
     hol--;
-    konyv[hol].style.transform ="rotateY(0deg) rotateX(-10deg)";
-    konyv[hol].getElementsByTagName("div")[0].style.transform = "rotateY(0deg) rotateX(-10deg)";
-    konyv[hol].getElementsByTagName("div")[1].style.transform = "rotateY(180deg) rotateX(-10deg)";
+    konyv[hol].style.transform ="rotateY(0deg)";
+    konyv[hol].getElementsByTagName("div")[0].style.transform = "rotateY(0deg)";
+    konyv[hol].getElementsByTagName("div")[1].style.transform = "rotateY(-180deg)";
     konyv[hol].style.zIndex = -hol;
   }
 
   // document.getElementsByTagName("div")[nth].style.transform="rotateY(-180deg) rotateX(10deg)";
-  console.log(nth, hol);
 }
