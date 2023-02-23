33716d2e332e3e5ebdab95be6fec2b92f25445c4
malackapite <veress.marcell@protonmail.com>
2023-02-24 00:16:45 +0100

    :cat2::dash:

---
+++ import.js
@@ -0,0 +1,19 @@
+import json from "./tartalom.json" assert { type: "json" };
+
+document.body.innerHTML+=`<div id="konyv"></div>`
+for (let ix = 0; ix < json.oldalak.length; ix++) {
+    document.getElementById("konyv").innerHTML+=`
+    <div class="oldal" onclick="lapoz(${ix})" style="z-index: ${-ix};">
+            <div class="jobb">
+               ${json.oldalak[ix].jobb}
+            </div>
+            <div class="bal" style="transform: rotateY(-180deg);">
+            ${json.oldalak[ix].bal}
+            </div>
+    </div>`
+}
+
+for(var ix=1; ix < document.getElementsByClassName("oldal").length-1; ix++){
+    document.getElementsByClassName("jobb")[document.getElementsByClassName("oldal").length-1-ix].style.width=400-2*ix+"px";
+    document.getElementsByClassName("bal")[ix].style.width=400-2*ix+"px";
+  }
--- js.js
+++ js.js
@@ -1,12 +1,8 @@
 hol = 0;
-
-for(var ix=1; ix < document.getElementsByClassName("oldal").length-1; ix++){
-  document.getElementsByClassName("jobb")[document.getElementsByClassName("oldal").length-1-ix].style.width=400-2*ix+"px";
-  document.getElementsByClassName("bal")[ix].style.width=400-2*ix+"px";
-}
-
 konyv=document.getElementsByClassName("oldal")
+
 function lapoz(nth) {
+    
   if (nth == hol) {
     konyv[hol].style.transform = "rotateY(180deg)";
     konyv[hol].getElementsByTagName("div")[0].style.transform = "rotateY(-180deg)";
@@ -20,6 +16,4 @@ function lapoz(nth) {
     konyv[hol].getElementsByTagName("div")[1].style.transform = "rotateY(-180deg)";
     konyv[hol].style.zIndex = -hol;
   }
-
-  // document.getElementsByTagName("div")[nth].style.transform="rotateY(-180deg) rotateX(10deg)";
 }
